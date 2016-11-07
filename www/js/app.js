779cfb79de2c6edee0be4aa732ef4e98d6fab355
joydroid <elaine_peng@yahoo.com>
2016-11-07 12:27:35 -0800

    latest

--- www/js/app.js
+++ www/js/app.js
@@ -2,10 +2,13 @@
 
   var app = angular.module('myreddit', ['ionic', 'angularMoment']);
 
-  app.controller('RedditCtrl', function ($scope, $http) {
+  app.controller('RedditCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
     $scope.stories = [];
 
-    function loadStories(params, callback) {
+    function loadStories(params) {
+      var d = $q.defer();
+      var promise = d.promise;
+
       var stories = [];
       $http.get('https://www.reddit.com/r/android/new/.json', { 'params': params })
         .success(function (response) {
@@ -16,33 +19,53 @@
             }
             stories.push(story);
           });
-          callback(stories)
+          d.resolve(stories);
+        })
+        .error(function(error) {
+          d.reject(error);
         });
+      promise.success = function(fn) {
+        promise.then(fn);
+        return promise;
+      }
+      promise.error = function(fn) {
+        promise.then(null, fn);
+        return promise;
+      }
+      return promise;
     };
     $scope.loadOlderStories = function () {
+      console.log("loadOlderStories");
       var params = {};
       if ($scope.stories.length > 0) {
         params['after'] = $scope.stories[$scope.stories.length - 1].name;
       }
-      loadStories(params, function (olderStories) {
+      var promise = loadStories(params);
+      promise.then(function (olderStories) {
         $scope.stories = $scope.stories.concat(olderStories);
+      }).catch(function(error) {
+        console.log(error);
+      }).finally(function () {
         $scope.$broadcast('scroll.infiniteScrollComplete');
       });
     };
     $scope.loadNewerStories = function () {
+      console.log("loadNewerStories");
       var params = {};
       if ($scope.stories.length > 0) {
         params['before'] = $scope.stories[0].name;
       }
-      loadStories(params, function (newerStories) { 
+      var promise = loadStories(params);
+      promise.then(function (newerStories) {
         $scope.stories = newerStories.concat($scope.stories);
+      }).finally(function () {
         $scope.$broadcast('scroll.refreshComplete');
       });
     };
     $scope.openLink = function(link) {
       window.open(link, '_blank');
     };
-  });
+  }]);
 
   app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
